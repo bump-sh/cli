efb13e03ba36d0aba512012497ac5c7d79facb14
Paul B <paulr@bump.sh>
2021-04-12 12:14:12 +0200

    ci: Add a Github Action to launch basic dev checks

---
+++ tests/integration.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const spawn = require('cross-spawn');
+const cmd = './bin/run';
+const tests = [['--version'], ['--help']];
+
+for (let i = 0; i < tests.length; i++) {
+  const result = spawn.sync(cmd, tests[i]);
+
+  console.log(`> ${cmd} ${tests[i]}`);
+  if (result.status) {
+    console.log(result.stderr.toString());
+    process.exit(result.status);
+  }
+  console.log(result.stdout.toString());
+}
