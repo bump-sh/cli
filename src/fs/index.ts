f430e8d8b8e3a912ece81f458346e5d7b2276073
Paul B <paulr@bump.sh>
2021-04-15 16:06:56 +0200

    core: use official specs schemas to ensure supported formats
    We only support:
    
    - AsyncAPI
      - 2.0.x
    - OpenAPI
      - 2.0.x
      - 3.0.x
      - 3.1.x

--- src/fs/index.ts
+++ src/fs/index.ts
@@ -3,61 +3,7 @@ import { promisify } from 'util';
 import * as YAML from 'yaml';
 import axios from 'axios';
 
-class UnsupportedFormat extends Error {
-  constructor() {
-    super('Unsupported API specification.');
-  }
-}
-class API {
-  definition!: Spec;
-  version!: string;
-  spec!: string;
-
-  constructor(content: Spec) {
-    if (!(content instanceof Object)) {
-      throw new UnsupportedFormat();
-    }
-
-    if ('openapi' in content) {
-      this.spec = 'OpenAPI';
-      this.version = content.openapi;
-    } else if ('asyncapi' in content) {
-      this.spec = 'AsyncAPI';
-      this.version = content.asyncapi;
-    } else {
-      throw new UnsupportedFormat();
-    }
-
-    this.definition = content;
-  }
-}
-
-type Spec = OpenAPI | AsyncAPI;
-
-// http://spec.openapis.org/oas/v3.1.0#oasObject
-interface OpenAPI {
-  readonly openapi: string;
-  readonly info: string;
-  readonly servers?: Record<string, unknown>;
-  readonly paths?: Record<string, unknown>;
-  readonly webhooks?: Record<string, unknown>;
-  readonly components?: Record<string, unknown>;
-  readonly security?: Record<string, unknown>;
-  readonly tags?: Record<string, unknown>;
-  readonly externalDocs?: Record<string, unknown>;
-}
-
-// https://www.asyncapi.com/docs/specifications/2.0.0#A2SObject
-interface AsyncAPI {
-  readonly asyncapi: string;
-  readonly id?: string;
-  readonly info: string;
-  readonly servers?: Record<string, unknown>;
-  readonly channels: Record<string, unknown>;
-  readonly components?: Record<string, unknown>;
-  readonly tags?: Record<string, unknown>;
-  readonly externalDocs?: Record<string, unknown>;
-}
+import { API } from '../definitions';
 
 async function fetch(path: string): Promise<string> {
   if (new RegExp('^https?://').test(path)) {
