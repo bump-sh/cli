723946eb32dbf7b0489c2c5206cf51c40e279f08
Paul B <paulr@bump.sh>
2021-09-10 01:35:20 +0200

    previews: add a '--live' option to be able to do live previews
    This feature adds a new flag `--live` to the `bump preview` command,
    to continously update a preview at each file save.
    
    It's very helpful when you are writing your API definition file and
    want to see a live preview.

--- src/commands/preview.ts
+++ src/commands/preview.ts
@@ -4,6 +4,9 @@ import { fileArg } from '../args';
 import { cli } from '../cli';
 import { PreviewResponse, PreviewRequest } from '../api/models';
 
+import { watch } from 'fs';
+import { Mutex } from 'async-mutex';
+
 export default class Preview extends Command {
   static description = 'create a documentation preview from the given file or URL';
 
@@ -15,6 +18,9 @@ export default class Preview extends Command {
 
   static flags = {
     help: flags.help({ char: 'h' }),
+    live: flags.live({
+      description: 'Generate a preview each time you save the given file',
+    }),
     open: flags.open({ description: 'Open the generated preview URL in your browser' }),
   };
 
@@ -22,26 +28,74 @@ export default class Preview extends Command {
 
   async run(): Promise<void> {
     const { args, flags } = this.parse(Preview);
-    const [definition, references] = await this.prepareDefinition(args.FILE);
 
-    cli.action.start("* Let's render a preview on Bump");
+    if (flags.live) {
+      await this.waitForChanges(args.FILE, flags.open);
+    } else {
+      await this.preview(args.FILE, flags.open);
+    }
+
+    return;
+  }
+
+  async preview(
+    file: string,
+    open = false,
+    currentPreview: PreviewResponse | undefined = undefined,
+  ): Promise<PreviewResponse> {
+    const [definition, references] = await this.prepareDefinition(file);
+
+    if (!currentPreview) {
+      cli.action.start("* Let's render a preview on Bump");
+    }
 
     const request: PreviewRequest = {
       definition,
       references,
     };
-    const response: { data: PreviewResponse } = await this.bump.postPreview(request);
+    const response: { data: PreviewResponse } = currentPreview
+      ? await this.bump.putPreview(currentPreview.id, request)
+      : await this.bump.postPreview(request);
 
-    cli.action.stop();
+    if (!currentPreview) {
+      cli.action.stop();
+      cli.styledSuccess(
+        `Your preview is visible at: ${response.data.public_url} (Expires at ${response.data.expires_at})`,
+      );
+    }
 
-    cli.styledSuccess(
-      `Your preview is visible at: ${response.data.public_url} (Expires at ${response.data.expires_at})`,
-    );
-
-    if (flags.open && response.data.public_url) {
+    if (open && response.data.public_url) {
       await cli.open(response.data.public_url);
     }
 
-    return;
+    return response.data;
+  }
+
+  async waitForChanges(file: string, open: boolean): Promise<void> {
+    const mutex = new Mutex();
+    let currentPreview: PreviewResponse | undefined = undefined;
+
+    cli.action.start(`Waiting for changes on file ${file}...`);
+
+    watch(file, async () => {
+      if (!mutex.isLocked()) {
+        const release = await mutex.acquire();
+        const firstOpen = !currentPreview && open;
+
+        this.preview(file, firstOpen, currentPreview)
+          .then((preview) => {
+            currentPreview = currentPreview || preview;
+            cli.action.start(`Waiting for changes on file ${file}`);
+          })
+          .catch((err) => {
+            this.warn(err);
+          })
+          .finally(() => {
+            setTimeout(() => {
+              release();
+            }, 1000); // Prevent previewing faster than once per second
+          });
+      }
+    });
   }
 }
