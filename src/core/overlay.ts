3534f0f664dc4727d2b37ce72e0f7d147691a379
Paul B. <paulr@bump.sh>
2024-08-02 09:24:47 +0200

    fix(overlay): backport of an upstream fix for remove action
    This commit is a backport of
    https://github.com/lornajane/openapi-overlays-js/pull/13/files.
    
    To fix the issue of applying `remove` action on a target which is a
    list. Before the fix it would remove only the first occurence of the
    target. With this fix it removes all the occurences of the target
    JSONPath.

--- src/core/overlay.ts
+++ src/core/overlay.ts
@@ -1,64 +1,81 @@
-// WIP @github.com/lornajane/openapi-overlays-js
-//
-// I couldn't get the upstream lib to be imported properly due to some
-// issues with ESM module imports so this is file was copied from
-// github.com/lornajane/openapi-overlays-js and has been adapted to
-// make our Typescript build happy.
-//
-// If you make any changes here, please also make them upstream.
+import debug from 'debug';
 import jsonpath from 'jsonpath';
 import mergician from 'mergician';
 import { JSONSchema4Object } from 'json-schema';
 
 import { APIDefinition, OpenAPIOverlay } from '../definition';
 
-export function applyOverlay(spec: APIDefinition, overlay: OpenAPIOverlay) {
-  // Use jsonpath.apply to do the changes
-  if (overlay.actions && overlay.actions.length >= 1)
-    overlay.actions.forEach((a) => {
-      const action = a as JSONSchema4Object;
-      if (!action.target) {
-        process.stderr.write('Action with a missing target\n');
-        return;
-      }
-      const target = action.target as string;
-      // Is it a remove?
-      if (action.hasOwnProperty('remove')) {
-        while (true) {
-          const path = jsonpath.paths(spec, target, 1);
-          if (path.length == 0) {
-            break;
-          }
-          const parent = jsonpath.parent(spec, target);
-          const thingToRemove = path[0][path[0].length - 1];
-          if (Array.isArray(parent)) {
-            parent.splice(thingToRemove as number, 1);
-          } else {
-            delete parent[thingToRemove];
-          }
+export class Overlay {
+  // WIP @github.com/lornajane/openapi-overlays-js
+  //
+  // I couldn't get the upstream lib to be imported properly due to
+  // some issues with ESM module imports so this is method was copied
+  // from github.com/lornajane/openapi-overlays-js and has been
+  // adapted to make our Typescript build happy.
+  //
+  // If you make any changes here, PLEASE ALSO MAKE THEM UPSTREAM.
+  public run(spec: APIDefinition, overlay: OpenAPIOverlay): APIDefinition {
+    // Use jsonpath.apply to do the changes
+    if (overlay.actions && overlay.actions.length >= 1)
+      overlay.actions.forEach((a) => {
+        const action = a as JSONSchema4Object;
+        if (!action.target) {
+          process.stderr.write('Action with a missing target\n');
+          return;
         }
-      } else {
-        try {
-          // It must be an update
-          jsonpath.apply(spec, target, (chunk) => {
-            if (typeof chunk === 'object' && typeof action.update === 'object') {
-              if (Array.isArray(chunk) && Array.isArray(action.update)) {
-                return chunk.concat(action.update);
-              } else {
-                // Deep merge objects using a module (built-in spread operator is only shallow)
-                const merger = mergician({ appendArrays: true });
-                return merger(chunk, action.update);
-              }
-            } else {
-              return action.update;
+        const target = action.target as string;
+        // Is it a remove?
+        if (action.hasOwnProperty('remove')) {
+          while (true) {
+            const path = jsonpath.paths(spec, target);
+            if (path.length == 0) {
+              break;
             }
-          });
-        } catch (ex) {
-          process.stderr.write(`Error applying overlay: ${(ex as Error).message}\n`);
-          //return chunk
+            const parent = jsonpath.parent(spec, target);
+            const thingToRemove = path[0][path[0].length - 1];
+            if (Array.isArray(parent)) {
+              parent.splice(thingToRemove as number, 1);
+            } else {
+              delete parent[thingToRemove];
+            }
+          }
+        } else {
+          try {
+            // It must be an update
+            // Deep merge objects using a module (built-in spread operator is only shallow)
+            const merger = mergician({ appendArrays: true });
+            if (target === '$') {
+              // You can't actually merge an update on a root object
+              // target with the jsonpath lib, this is just us merging
+              // the given update with the whole spec.
+              spec = merger(spec, action.update);
+            } else {
+              jsonpath.apply(spec, target, (chunk) => {
+                if (typeof chunk === 'object' && typeof action.update === 'object') {
+                  if (Array.isArray(chunk) && Array.isArray(action.update)) {
+                    return chunk.concat(action.update);
+                  } else {
+                    return merger(chunk, action.update);
+                  }
+                } else {
+                  return action.update;
+                }
+              });
+            }
+          } catch (ex) {
+            process.stderr.write(`Error applying overlay: ${(ex as Error).message}\n`);
+            //return chunk
+          }
         }
-      }
-    });
+      });
 
-  return spec;
+    return spec;
+  }
+
+  // Function signature type taken from @types/debug
+  // Debugger(formatter: any, ...args: any[]): void;
+  /* eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any */
+  d(formatter: any, ...args: any[]): void {
+    return debug(`bump-cli:core:overlay`)(formatter, ...args);
+  }
 }
