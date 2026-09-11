b2c57f22b4757df29f276b363aff3fa4992a8307
Paul B. <paulr@bump.sh>
2026-09-11 12:29:02 +0200

    arazzo: add 1.1.0 spec schema to allow upload of v1.1 arazzo documents
    This commit adds the arazzo spec v1.1 to be able to parse and upload
    arazzo v1.1 documents to the Bump.sh platform.

--- src/core/schemas/arazzo-schemas/index.ts
+++ src/core/schemas/arazzo-schemas/index.ts
@@ -3,9 +3,11 @@ import type {JSONSchema7} from 'json-schema'
 // Spec definitions are copied from the official
 // spec repo https://spec.openapis.org/arazzo/
 import schemaV10 from './v1.0/schema.json' with {type: 'json'}
+import schemaV11 from './v1.1/schema.json' with {type: 'json'}
 
 export default {
   schemas: {
     '1.0': schemaV10 as JSONSchema7,
+    '1.1': schemaV11 as JSONSchema7,
   },
 }
